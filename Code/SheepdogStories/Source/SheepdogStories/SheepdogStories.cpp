f1586f3e77f36a2752f8b3a56d273fb4514128b2
Fan-tome14 <96857821+Fan-tome14@users.noreply.github.com>
2025-11-18 12:14:25 -0500

    Commit Initial Code

---
+++ Code/SheepdogStories/Source/SheepdogStories/SheepdogStories.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SheepdogStories.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SheepdogStories, "SheepdogStories" );
